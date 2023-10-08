cbb013a678936f05e651a85bb241ffe010283486
AbhishekChorotiya <2020kuec2033@iiitkota.ac.in>
2023-10-09 00:32:03 +0530

    starting after 5 months

--- backend/models/courses.js
+++ backend/models/courses.js
@@ -1,27 +1,34 @@
-const mongoose = require('mongoose');
-const jwt = require('jsonwebtoken')
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
 
 const courseSchema = new mongoose.Schema({
-    CourseId:{
+  CourseId: {
+    type: String,
+  },
+  CourseName: {
+    type: String,
+  },
+  CourseCredit: {
+    type: String,
+  },
+  Branch: {
+    type: String,
+  },
+  Semester: {
+    type: String,
+  },
+  FacultyId: {
+    type: String,
+  },
+  students: [
+    {
+      studendId: {
         type: String,
+      },
     },
-    CourseName : {
-        type: String,
-    },
-    CourseCredit: {
-        type: String,
-    },
-    Branch: {
-        type: String
-    },
-    Semester: {
-        type: String,
-    },
-    FacultyId: {
-        type: String
-    }
-})
+  ],
+});
 
-const courseObj = mongoose.model('courses',courseSchema)
+const courseObj = mongoose.model("courses", courseSchema);
 
 module.exports = courseObj;
